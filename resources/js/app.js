9c2f5df30079330271f268ccb57def6d69f2b62d
Fauzanhidayat <fauzanhidayat773@gmail.com>
2026-09-03 09:17:12 +0700

    update(app): import hitung tagihan & customer search alpine

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
-//
+import Alpine from 'alpinejs';
+import focus from '@alpinejs/focus'
+import { formTagihan } from './components/form-tagihan';
+import { customerSearch } from './components/customer-search';
+
+window.Alpine = Alpine;
+
+Alpine.plugin(focus)
+Alpine.data('formTagihan', formTagihan);
+Alpine.data('customerSearch', customerSearch);
+
+Alpine.start();
