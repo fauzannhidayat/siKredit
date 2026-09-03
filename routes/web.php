4d99f70dfdb63d9ce559b9d8e98908e7df2973a3
Fauzanhidayat <fauzanhidayat773@gmail.com>
2026-09-03 09:18:06 +0700

    update(routes): add pengajuan routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\PengajuanController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pengajuan', [PengajuanController::class, 'index'])->name('pengajuan.index');
+Route::post('/pengajuan', [PengajuanController::class, 'store'])->name('pengajuan.store');
+Route::patch('/pengajuan/{pengajuan}/status/{status}', [PengajuanController::class, 'updateStatus'])
+    ->name('pengajuan.update-status');
+
+Route::get('/customer/search', [CustomerController::class, 'search'])->name('customer.search');
